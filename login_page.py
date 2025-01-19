c332abd9a9a20202434e12083e04f9361283c7eb
Mohanasangeetha <134695215+mohanasan@users.noreply.github.com>
2025-01-19 11:32:47 +0530

    Update login_page.py

--- login_page.py
+++ login_page.py
@@ -11,18 +11,34 @@ def login_page():
     <style>
     /* Apply background image to the main content area */
     .main {
-        background-image: url("https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDI0LTExL3Jhd3BpeGVsb2ZmaWNlNF93aGl0ZV9hbmRfc2lsdmVyX3NpbXBsZV9wbGFpbl9ncmFkaWVudF9iYWNrZ3JvdV8xMTgwZTY5Yy0yNjczLTQ2MTItYmFhNC1jMGFiMDFiODRmYzIuanBn.jpg");  
+        background-image: url("https://www.ajhospital.in/storage/files/news/Blog/01.jpg");  
         background-size: cover;
         background-position: center;
         background-repeat: no-repeat;
+        background-color: rgba(255, 255, 255, 0.6);
+        background-blend-mode: overlay;
     }
     </style>
     """,
     unsafe_allow_html=True
     )
+    #add space
+    st.write("")
+    st.write("")
+    st.write("")
+    st.write("")
+    st.write("")
+    st.write("")
     with st.form(key="login_form"):
         # Title
-        st.title("Login Page")
+        st.markdown(
+            """
+            <div style="text-align: center; color: red;">
+                <h1 style="font-size: 50px; color:blue;">Login Here !!</h1>
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
 
         # Email and Password inputs
         email = st.text_input("Email")
@@ -31,7 +47,7 @@ def login_page():
         # Submit button inside the form
         col1,col2,col3=st.columns([1,2,1])
         with col1:
-            if st.form_submit_button("Login"):
+            if st.form_submit_button("Login",type='primary'):
                 if authenticate_user(email, password):
                     st.success(f"Login successful. Welcome {email}!")
                     st.session_state["logged_in"] = True
@@ -41,5 +57,5 @@ def login_page():
                 else:
                     st.error("Invalid email or password.")
         with col3:
-            if st.form_submit_button("Create an account?"):
+            if st.form_submit_button("Create an account?",type='primary'):
                 navigate_to_page("signup")
